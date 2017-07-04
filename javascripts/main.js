36af58a4e6039408a2cf2a54b9ec365df8ca939e
Ed Brannin <edbrannin@gmail.com>
2017-07-04 13:21:37 -0400

    Add Vuejs, show a list of past results

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,35 @@
-$(document).ready(function(){
-	$('#reader').html5_qrcode(function(data){
-			$('#read').html(data);
-		},
-		function(error){
-			$('#read_error').html(error);
-		}, function(videoError){
-			$('#vid_error').html(videoError);
-		}
-	);
+var app = new Vue({
+  el: '#app',
+  mounted: function() {
+    const t = this;
+    $('#reader').html5_qrcode(function(data){
+      $('#read').html(data);
+      console.log("Got value:", data);
+      t.newValue(data);
+    },
+    function(error){
+      t.readError = error;
+    }, function(videoError){
+      t.vidError = error;
+    });
+  },
+  methods: {
+    newValue: function(v) {
+      console.log("Adding value:", v);
+      console.log("This:", this);
+      if (
+        v != undefined
+        && v != this.currentValue
+        && ! this.values.includes(v)
+      ) {
+        this.values.push(v);
+      }
+    }
+  },
+  data: {
+    currentValue: undefined,
+    readError: undefined,
+    vidError: undefined,
+    values: []
+  }
 });
